f37957d068134086439603984440fb78bef897d7
Lucky Cungwa <36919645+luckycungwa@users.noreply.github.com>
2023-11-03 22:50:57 +0200

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Have = () => {
+  return (
+    <div>
+      {/* Add your component content here */}
+      <p>My Home page</p>
+      
+    </div>
+  );
+};
+
+export default Have;
