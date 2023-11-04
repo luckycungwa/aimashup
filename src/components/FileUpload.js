6cb55fc72d22e535c60c7fc584252a4c7fca433a
Tshikamisava <lhlongwane81@gmail.com>
2023-11-04 02:22:04 +0000

    file size

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Dropzone from "react-dropzone";
 
 const FileUpload = () => {
-  const [files, setFiles] = useState("");
+  const [files, setFiles] = useState(null);
   const [extractedText, setExtractedText] = useState("");
 
   const onDrop = async (acceptedFiles) => {
@@ -14,8 +14,8 @@ const FileUpload = () => {
     formData.append("image", file);
 
     // Replace with your Azure Computer Vision API endpoint and API key
-    const endpoint = "https://aimashupvision.cognitiveservices.azure.com/";         //endpoint key from th azure sevrice
-    const apiKey = "3c85d2abaa2e4eaf8d2453443def9f79";                                //API key from azure
+    const endpoint = "https://aimashupvision.cognitiveservices.azure.com";
+    const apiKey = "3c85d2abaa2e4eaf8d2453443def9f79";
 
     try {
       const response = await axios.post(`${endpoint}/read/analyze`, formData, {
@@ -25,53 +25,48 @@ const FileUpload = () => {
         },
       });
 
-      // Extracted text from the API response
       setExtractedText(response.data.text);
     } catch (error) {
-      // Handle errors
       console.error("Error:", error);
     }
   };
 
   return (
     <>
-    <div className="uploadSection">
-      <div>
-      <p className="title3">Upload your CV</p>
-      
-      </div>
-      <div className="tipContainer">
-      <Dropzone onDrop={onDrop}>
-          {({ getRootProps, getInputProps }) => (
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              {/* DROP FILE SECTION */}
-              <div className="tipContainer">
-              <div>
-              <img className="uploadIcon" src="noun-upload.svg" alt="Icon" />
+      <div className="uploadSection">
+        <div>FileUpload</div>
+        <div className="tipContainer">
+          <Dropzone onDrop={onDrop}>
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <div className="tipContainer">
+                  <div>
+                    <img className="uploadIcon" src="noun-upload.svg" alt="Icon" />
+                  </div>
+                  <p className="tipText">
+                    Drag 'n' drop some files here, or click to select files
+                  </p>
+                </div>
               </div>
-                <p className="tipText">
-                  Drag 'n' drop some files here, or click to select files
-                </p>
-              </div>
-            </div>
-          )}
-        </Dropzone>
-      </div>
-
-      {files && (
-        <div>
-          <h3 className="title4">File Details:</h3>
-          <p className="title5"> {files.name}</p> <p className="lightText">Size: {files.size}</p>
+            )}
+          </Dropzone>
         </div>
-      )}
 
-      {extractedText && (
-        <div>
-          <h3>Extracted Text:</h3>
-          <p>{extractedText}</p>
-        </div>
-      )}
+        {files && (
+          <div>
+            <h3>File Details:</h3>
+            <p>Name: {files.name}</p>
+            <p>Size: {files.size ? `${(files.size / 1024 / 1024).toFixed(2)} MB` : ''}</p>
+          </div>
+        )}
+
+        {extractedText && (
+          <div>
+            <h3>Extracted Text:</h3>
+            <p>{extractedText}</p>
+          </div>
+        )}
       </div>
     </>
   );
