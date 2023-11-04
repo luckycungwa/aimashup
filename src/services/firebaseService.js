f79f8b4335b5dd35e465a1940ff072a137611768
Lucky Cungwa <36919645+luckycungwa@users.noreply.github.com>
2023-11-04 04:40:09 +0200

    intergrating firebase | database

---
+++ src/services/firebaseService.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAj7drd3CjVdpIpwrPxb7HJkqiiNpGstrs",
+  authDomain: "yesproject-6055e.firebaseapp.com",
+  projectId: "yesproject-6055e",
+  storageBucket: "yesproject-6055e.appspot.com",
+  messagingSenderId: "469621518367",
+  appId: "1:469621518367:web:54e3084a469ec57e03252a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
